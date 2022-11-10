1e50e7b4e9052696e874a8de236d15b9ae67d18f
soohofather <enthchal@gmail.com>
2022-11-10 14:05:40 +0900

    feat : articles CRUD 구현

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ArticleForm
+from .models import Article
 
 # Create your views here.
+
+
+def index(request):
+
+    articles = Article.objects.order_by('-pk')
+
+    context = {
+        'articles': articles,
+    }
+
+    return render(request, 'articles/index.html', context)
+
+def create(request):
+
+    if request.method == "POST":
+        article_form = ArticleForm(request.POST, request.FILES)
+        if article_form.is_valid():
+            article_form.save()
+            return redirect("articles:index")
+    else:
+        article_form = ArticleForm()
+    context = {
+        "article_form": article_form,
+    }
+    return render(request, "articles/create.html", context)
+
+def detail(request, pk):
+
+    return render(request, "articles/detail.html")
+
+
+def delete(request, pk):
+
+    article = Article.objects.get(pk=pk)
+
+    article.delete()
+
+    return redirect("articles:index")
+
+
+def update(request, pk):
+
+    article = Article.objects.get(pk=pk)
+
+    if request.method == "POST":
+        article_form = ArticleForm(request.POST, request.FILES, instance=article)
+        if article_form.is_valid():
+            article_form.save()
+            return redirect("articles:index")
+    else:
+        article_form = ArticleForm(instance=article)
+    context = {
+        "article_form": article_form,
+    }
+    return render(request, "articles/update.html", context)
