34461b8b6287df7b9fb0a197fb5e303ac4d6c90b
jincde <asdzxc@kakao.com>
2022-11-23 10:25:26 +0900

    feat : 도메인 테스트

--- PJT/settings.py
+++ PJT/settings.py
@@ -55,7 +55,8 @@ else:
 
 ALLOWED_HOSTS = [
     "Chickenkurlybean-env.eba-jbwiaqp9.ap-northeast-1.elasticbeanstalk.com",
-    "kurly.shop", # 예시입니다. 본인 URL로 해주세요.
+    "kurly.shop",
+    "spjt2.shop", # 예시입니다. 본인 URL로 해주세요.
     "127.0.0.1",
     "localhost",
     ]
